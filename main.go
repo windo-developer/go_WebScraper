35c579f7896bd6818eabf9e8803eeb81f4ccc644
windo-developer <yui050505@gmail.com>
2021-06-24 18:08:33 +0900

    csv fix

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -20,11 +23,32 @@ type extractedJob struct {
 var baseURL string = "https://kr.indeed.com/jobs?q=unreal"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+
+	writeJobs(jobs)
+	fmt.Println("extracted: ", len(jobs))
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("status code error: %d %s", res.StatusCode, res.Status)
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
@@ -44,7 +68,23 @@ func getPages() int {
 	return pages
 }
 
-func getPage(page int) {
+func extractJob(job *goquery.Selection) extractedJob {
+	id, _ := job.Attr("data-jk")
+	title := cleanString(job.Find(".title>a").Text())
+	location := cleanString(job.Find(".sjcl").Text())
+	salary := cleanString(job.Find(".salaryText").Text())
+	summary := cleanString(job.Find(".summary").Text())
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
+}
+
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageUrl := baseURL + "&start=" + strconv.Itoa(page*10)
 	res, err := http.Get(pageUrl)
 	checkError(err)
@@ -58,25 +98,27 @@ func getPage(page int) {
 
 	jobCards := doc.Find(".jobsearch-SerpJobCard")
 	jobCards.Each(func(i int, s *goquery.Selection) {
-		id, exists := s.Attr("data-jk")
-		title := s.Find(".title>a").Text()
-		location := s.Find(".sjcl").Text()
-		fmt.Println(id, exists, title, location)
+		job := extractJob(s)
+		jobs = append(jobs, job)
 	})
+
+	return jobs
 }
 
-func checkError(err error) {
-	if err != nil {
-		log.Fatalln(err)
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkError(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "Title", "Location", "Salary", "Summary"}
+	wErr := w.Write(headers)
+	checkError(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkError(jwErr)
 	}
-}
-
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("status code error: %d %s", res.StatusCode, res.Status)
-	}
-}
-
-func cleanString(str string) {
-
 }
